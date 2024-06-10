30109bfe1571a6495b061232156d3c28684ebc4f
Drbaemsu <162392214+Drbaemsu@users.noreply.github.com>
2024-06-11 00:14:15 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 환경 변수 설정
-export GRADIO_FRPC_PATH=/usr/local/lib/python3.9/site-packages/gradio/frpc_linux_amd64_v0.2
+export GRADIO_FRPC_PATH=/app/frpc_linux_amd64_v0.2
 
 # Gradio 애플리케이션 실행
 python app.py
