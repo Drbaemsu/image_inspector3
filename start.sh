d1a6b5c1d3dbd8787dbc52b5eb228a3604405e19
Drbaemsu <162392214+Drbaemsu@users.noreply.github.com>
2024-06-10 20:50:34 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+
 # 다운로드 및 파일 이동
 wget -O /tmp/frpc_linux_amd64_v0.2 https://cdn-media.huggingface.co/frpc-gradio-0.2/frpc_linux_amd64
 chmod +x /tmp/frpc_linux_amd64_v0.2
+
+# 필요시 디렉토리 생성
 mkdir -p /usr/local/lib/python3.9/site-packages/gradio
+
+# 파일 이동
 mv /tmp/frpc_linux_amd64_v0.2 /usr/local/lib/python3.9/site-packages/gradio/
 
 # Gradio 애플리케이션 실행
 python app.py
+
